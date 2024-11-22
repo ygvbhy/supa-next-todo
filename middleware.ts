b80a92c6bf28f1856384a6dc1c8d6d4ecb59a56c
sypark <sangyune.park@k-futuretech.com>
2024-11-22 16:56:18 +0900

    6.5 Middleware ex) 쿠키 카운터

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,16 @@
 import { NextRequest, NextResponse } from "next/server";
 
+const COOKIE_COUNTER = "cookie-counter";
+
 export function middleware(request: NextRequest) {
   const response = NextResponse.next();
 
-  console.log("middleware 통과");
+  if (request.cookies.get(COOKIE_COUNTER)?.value) {
+    const prev = request.cookies.get(COOKIE_COUNTER)?.value;
+    response.cookies.set(COOKIE_COUNTER, `${Number(prev) + 1}`);
+  } else {
+    response.cookies.set(COOKIE_COUNTER, "1");
+  }
 
   return response;
 }
