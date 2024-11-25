0609a02f00638dc608aad1c3de41b5d068d0c102
sypark <sangyune.park@k-futuretech.com>
2024-11-25 16:28:53 +0900

    8.7 테스트 및 빌드 오류 수정

--- app/api/todo/route.ts
+++ app/api/todo/route.ts
@@ -1,11 +1,11 @@
-import { getTodoAction } from "@/actions/todo/ping.action";
+// import { getTodoAction } from "@/actions/todo/ping.action";
 import { createServerSideClient } from "@/lib/supabase";
 import { NextResponse } from "next/server";
 
 export const GET = async () => {
   const supabase = await createServerSideClient();
 
-  const result = await getTodoAction();
+  // const result = await getTodoAction();
 
-  return NextResponse.json({ ...result });
+  // return NextResponse.json({ ...result });
 };
