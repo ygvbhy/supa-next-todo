c9fe36416b4f54e386bf96ca19ef6670211d6b9c
sypark <sangyune.park@k-futuretech.com>
2024-11-25 15:46:04 +0900

    8.6 사용자 정보 가져오기

---
+++ actions/auth/user.action.ts
@@ -0,0 +1,8 @@
+import { createServerSideClient } from "@/lib/supabase";
+
+export const getUser = async () => {
+  const supabase = await createServerSideClient();
+  const user = await supabase.auth.getUser();
+
+  return user?.data?.user;
+};
